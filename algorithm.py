81d4d7e6d6ecb1c0ce0652e3de16c445ee909747
bluejeans98 <jsk0821@kaist.ac.kr>
2022-05-29 20:49:18 +0900

    added names_corpus_filter

--- algorithm.py
+++ algorithm.py
@@ -8,32 +8,77 @@ from urllib.error import HTTPError
 from nltk.tokenize import sent_tokenize
 from nltk.tokenize import word_tokenize
 from nltk import ne_chunk, pos_tag
+import nltk.corpus
+
+
+def counter_filter(text, a ,b):
+    tokens = word_tokenize(text)
+    count_a, count_b = 0, 0
+    tagged_tokens = pos_tag(tokens)
+    for chunk in ne_chunk(tagged_tokens):
+        if hasattr(chunk, 'label') and chunk.label()=="PERSON":
+            name = ' '.join(c[0] for c in chunk)
+            if name in a:
+                count_a+=1
+            elif name in b:
+                count_b+=1
+    if(count_a==0 and count_b==0):
+        return ("FALSE","FALSE")
+    elif(count_a > count_b):
+        return ("TRUE", "FALSE")
+    else:
+        return ("FALSE", "TRUE")
+
+def names_corpus_filter(text, p, a, b, pre_result):
+    a_result = pre_result[0]
+    b_result = pre_result[1]
+
+    male = ['he','his','him']
+    female = ['she','her']
+
+    names = nltk.corpus.names
+    male_names = names.words('male.txt')
+    female_names = names.words('female.txt')
+
+    gender = 'male'
+    if p.lower() in male:
+        gender = 'male'
+    elif p.lower() in female:
+        gender = 'female'
+    
+    
+    a_first_name = a.split()[0]
+    b_first_name = b.split()[0]
+    if(gender == 'male'):
+        if(a_first_name in female_names and a_first_name not in male_names):
+            a_result = "FALSE"
+        if(b_first_name in female_names and b_first_name not in male_names):
+            b_result = "FALSE"
+    elif(gender == 'female'):
+        if(a_first_name in male_names and a_first_name not in female_names):
+            a_result = "FALSE"
+        if(b_first_name in male_names and b_first_name not in female_names):
+            b_result = "FALSE"
+    return (a_result, b_result)
+
+# def offset_filter(p_o, a_o, b_o, pre_result):
+#     a_result = pre_result[0]
+#     b_result = pre_result[1]
+#     if(p_o<a_o):
+#         a_result = "FALSE"
+#     if(p_o<b_o):
+#         b_result = "FALSE"
+#     return (a_result, b_result)
+    
 
 
 def algorithm(row,page):
     ID, text, p, p_o, a, a_o, a_c, b, b_o, b_c, url = row
     if(page==False):
-        male = ['he','his','him']
-        female = ['she','her']
-        tokens = word_tokenize(text)
-        count_a = 0
-        count_b = 0
-        tagged_tokens = pos_tag(tokens)
-        for chunk in ne_chunk(tagged_tokens):
-            if hasattr(chunk, 'label') and chunk.label()=="PERSON":
-                name = ' '.join(c[0] for c in chunk)
-                if name in a:
-                    count_a+=1
-                elif name in b:
-                    count_b+=1
-        print(count_a, count_b)
-        if(count_a==0 and count_b==0):
-            return ("FALSE","FALSE")
-        elif(count_a > count_b):
-            return ("TRUE", "FALSE")
-        else:
-            return ("FALSE", "TRUE")
-        
+        result = counter_filter(text, a, b)
+        result = names_corpus_filter(text, p, a, b, result)
+        # result = offset_filter(p_o, a_o, b_o, result)
+        return result
     elif(page==True):
         try:
             html = request.urlopen(url).read().decode('utf8')
